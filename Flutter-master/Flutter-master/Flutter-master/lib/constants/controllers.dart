d6c054c92efd8d10710204db6defb2fe23312284
walidmz <walid.marzouk@esprit.tn>
2024-01-03 10:43:28 +0100

    ad

--- Flutter-master/Flutter-master/Flutter-master/lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:quiz_backoffice/controllers/menu_controller.dart';
-import 'package:quiz_backoffice/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
